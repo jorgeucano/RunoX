19619d024535496e8499f93e1ad1c95224322dc4
Guillermo <guillermo.fitzpatrick@evolutionfinance.com>
2020-03-26 09:49:03 -0300

    nuevas cartas y mejoras de css y logica

--- cartas.js
+++ cartas.js
@@ -1,30 +1,46 @@
 
 window.onload = function() {
+    let cartas = ["0","1","2","3","4","5","6","7","8","9","&empty;","+2","&duarr;","+4"];
     let colores = ["verde","amarilla","azul","roja"];
     let html = "";
 
-    // Cartas de 1 a 9
-    for(i = 1;i<10;i++)
+    // Loop para generar las Cartas 
+    for(carta = 0;carta<cartas.length;carta++)
     {
+        // Genero un contenedor para que muestre de a 4 cartas
         html += `<div class='contenedor-cartas'>`;
 
-        // Por cada color de las Cartas
-        for(j = 0;j<4;j++) {
-            html += `
-                <div class='carta ${colores[j]}' data-value='${i}'>
-                    <div class='ellipse' data-value='${i}'>
-                    </div>
-                </div>`
+        // Loop por cada Color
+        for(color = 0;color<colores.length;color++) {
+
+            // GeneraHtmlCarta construye y devuelve el html de una carta en particular, 
+            // Recibe 2 parametros : carta y color, por ejemplo "1" y "amarilla"
+            html += GeneraHtmlCarta( cartas[carta], colores[color] )
+            this.console.log("html generado de carta: ",cartas[carta], colores[color])
             
         }
         html += `</div>`;
     }
 
-    // Termine de generar el html, voy a tratar de pegarlo en el nodo #cartas
+    // Una vez generado el html, se debe insertar en el nodo #cartas para mostrarlo en la pagina
     try {
         document.getElementById("cartas").innerHTML = html;
     }
     catch (e) {
-        console.log("no encuentro nodo con #cartas para insertar el html generado.")
+        console.log("No encuentro nodo #cartas para insertar el html generado.")
     }
+}
+
+function GeneraHtmlCarta( carta, color) {
+    italic =  ["&empty;","+2","&duarr;","+4"].indexOf(carta) > -1 ? "" : "italic";
+    maschica =  ["+2","+4","&duarr;"].indexOf(carta) > -1 ? "maschica" : "";
+    normal =  (maschica === "") ? "normal" : "";
+
+    let html = `
+        <div class='carta ${color} ${italic} ${maschica}' data-value='${carta}'>
+            <div class='ellipse ${normal} ${maschica}' data-value='${carta}'>
+            </div>
+        </div>`;
+    
+    return html;
 }
