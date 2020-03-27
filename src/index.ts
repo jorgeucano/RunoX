d514a75848de25c966902f54f178dfff88a942f2
Samuel BR <samuelb1311@gmail.com>
2020-03-27 14:54:17 -0500

    Generate Deck and RandomDeck implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-const es6ArrowFunction = () => console.log( 'it works' )
+import RandomDeck from "./utils/randomDeck/randomDeck";
 
-es6ArrowFunction()
+const cartas = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "+4", "flip", "skip"];
+const colores = ["verde", "amarilla", "azul", "roja"];
+
+let deck = new RandomDeck();
+colores.forEach((color) => {
+  cartas.forEach((carta) => {
+    const currentCard = `${carta}--${color}`;
+    deck.cards.push(currentCard, currentCard);
+  });
+});
+
+// Ahora cuando iteras deck, se genera un deck aleatorio nuevo
+// Forma 1 de Iteracion:
+console.log([...deck]);
+
+// Forma 2 de Iteracion:
+for (let card of deck) {
+  console.log(card);
+}
+
+// Pero los cards del deck siempre estan intactos (ordenados, como se pushearon):
+console.log(deck.cards);
