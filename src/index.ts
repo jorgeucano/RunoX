81f9e6247a4dbf08be05736d63f5f21d132b6174
Ernesto cardenas <ernesto.cardenas79@gmail.com>
2020-03-29 19:52:43 -0700

    Realizando algunas correcciones de textos para que se muestren todas las cartas, ajuste en el sprite para que se muestre el reversa verde

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
-import "./styles/styles.css";
+import './styles/styles.css';
 
-import RandomDeck from "./utils/randomDeck/randomDeck";
+import RandomDeck from './utils/randomDeck/randomDeck';
 
-const cartas = ["cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "mas-dos", "saltar", "reversa"];
-const cartasEspeciales = ["mas-cuatro", "comodin"]
-const colores = ["verde", "amarilla", "azul", "roja"];
+const cartas = [
+  'cero',
+  'uno',
+  'dos',
+  'tres',
+  'cuatro',
+  'cinco',
+  'seis',
+  'siete',
+  'ocho',
+  'nueve',
+  'mas-dos',
+  'saltar',
+  'reversa'
+];
+const cartasEspeciales = ['mas-cuatro', 'comodin'];
+const colores = ['verde', 'amarillo', 'azul', 'rojo'];
 
 let deck = new RandomDeck();
 deck.cards.push(...cartasEspeciales, ...cartasEspeciales);
-colores.forEach((color) => {
-  cartas.forEach((carta) => {
+colores.forEach(color => {
+  cartas.forEach(carta => {
     const currentCard = `${carta}--${color}`;
     deck.cards.push(currentCard, currentCard);
   });
@@ -23,42 +37,47 @@ const player1 = {
   name: 'Jorge',
   id: 'jorge1234',
   hand: [...randomDeck.splice(0, handsLength)],
-  pic: 'https://pbs.twimg.com/profile_images/1229508740510109697/Ww22knVc_400x400.jpg'
+  pic:
+    'https://pbs.twimg.com/profile_images/1229508740510109697/Ww22knVc_400x400.jpg'
 };
 
 const player2 = {
   name: 'Calel',
   id: 'calel1234',
   hand: [...randomDeck.splice(0, handsLength)],
-  pic: 'https://pbs.twimg.com/profile_images/1229508740510109697/Ww22knVc_400x400.jpg'
+  pic:
+    'https://pbs.twimg.com/profile_images/1229508740510109697/Ww22knVc_400x400.jpg'
 };
 
 const player3 = {
   name: 'Facu',
   id: 'Facu1234',
   hand: [...randomDeck.splice(0, handsLength)],
-  pic: 'https://pbs.twimg.com/profile_images/1196581886916747264/PaMavazA_400x400.jpg'
+  pic:
+    'https://pbs.twimg.com/profile_images/1196581886916747264/PaMavazA_400x400.jpg'
 };
 
 const player4 = {
-   name: 'Nicolas',
-   id: 'nikomendo',
-   hand: [...randomDeck.splice(0, handsLength)],
-   pic: 'https://pbs.twimg.com/profile_images/1106827262907899904/S1BXkb04_400x400.jpg'
+  name: 'Nicolas',
+  id: 'nikomendo',
+  hand: [...randomDeck.splice(0, handsLength)],
+  pic:
+    'https://pbs.twimg.com/profile_images/1106827262907899904/S1BXkb04_400x400.jpg'
 };
+
 const players = [player1, player2, player3, player4];
 
-players.forEach((player) => {
-  const div = document.createElement("div");
-  div.setAttribute("id", player.id);
+players.forEach(player => {
+  const div = document.createElement('div');
+  div.setAttribute('id', player.id);
 
-  player.hand.forEach((carta) => {
+  player.hand.forEach(carta => {
     const _hand = document.createElement('div');
-    _hand.setAttribute("class", `carta ${carta}`)
+    _hand.setAttribute('class', `carta ${carta}`);
     div.appendChild(_hand);
   });
-  
+
   _players?.appendChild(div);
-})
+});
 
 console.log(_players);
