4414f0604efa07b9216d8bb047bc0d9d001e38ce
jorgeucano <jorgeucano@gmail.com>
2020-03-31 17:52:05 -0300

    algunos arreglos y agregar carta a jugadores

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "./styles/styles.css";
 
 import RandomDeck from "./utils/randomDeck/randomDeck";
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const cartas = ["cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "mas-dos", "saltar", "reversa"];
 const cartasEspeciales = ["mas-cuatro", "comodin"]
-const colores = ["verde", "amarilla", "azul", "roja"];
+const colores = ["verde", "amarillo", "azul", "rojo"];
 
 let deck = new RandomDeck();
 deck.cards.push(...cartasEspeciales, ...cartasEspeciales);
@@ -17,33 +19,34 @@ colores.forEach((color) => {
 
 const _players = document.getElementById('players');
 const randomDeck = [...deck];
-const handsLength = randomDeck.length / 4; // 4 jugadores
+const handsLength = 7; // randomDeck.length / 4; // 4 jugadores
+
 
 const player1 = {
   name: 'Jorge',
   id: 'jorge1234',
-  hand: [...randomDeck.splice(0, handsLength)],
+  hand: [...randomDeck.splice((0*handsLength), handsLength)],
   pic: 'https://pbs.twimg.com/profile_images/1229508740510109697/Ww22knVc_400x400.jpg'
 };
 
 const player2 = {
   name: 'Calel',
   id: 'calel1234',
-  hand: [...randomDeck.splice(0, handsLength)],
+  hand: [...randomDeck.splice((1*handsLength), handsLength)],
   pic: 'https://pbs.twimg.com/profile_images/1229508740510109697/Ww22knVc_400x400.jpg'
 };
 
 const player3 = {
   name: 'Facu',
   id: 'Facu1234',
-  hand: [...randomDeck.splice(0, handsLength)],
+  hand: [...randomDeck.splice((2*handsLength), handsLength)],
   pic: 'https://pbs.twimg.com/profile_images/1196581886916747264/PaMavazA_400x400.jpg'
 };
 
 const player4 = {
    name: 'Nicolas',
    id: 'nikomendo',
-   hand: [...randomDeck.splice(0, handsLength)],
+   hand: [...randomDeck.splice((3*handsLength), handsLength)],
    pic: 'https://pbs.twimg.com/profile_images/1106827262907899904/S1BXkb04_400x400.jpg'
 };
 const players = [player1, player2, player3, player4];
@@ -51,14 +54,52 @@ const players = [player1, player2, player3, player4];
 players.forEach((player) => {
   const div = document.createElement("div");
   div.setAttribute("id", player.id);
-
   player.hand.forEach((carta) => {
     const _hand = document.createElement('div');
     _hand.setAttribute("class", `carta ${carta}`)
     div.appendChild(_hand);
   });
-  
   _players?.appendChild(div);
-})
+  setPlayerClicks(player.id);
+});
 
-console.log(_players);
+function setPlayerClicks(id: string) {
+  const _player = document.getElementById(id);
+  console.log(document.getElementById(id));
+  // @ts-ignore
+  fromEvent(_player, 'click')
+                .pipe(
+                  map(v => {
+                    // @ts-ignore
+                    return v.target.className;
+                  })
+                ).subscribe(
+                  (x: any) => {
+                    console.log(x);
+                  }
+                )
+}
+
+let nextCardFlag = handsLength * players.length;
+let currentPlayer = 0;
+const buttonNext = document.getElementById('button-next');
+// @ts-ignore
+const _next = fromEvent(buttonNext, 'click')
+                .subscribe(
+                  (x: any) => {
+                    console.log(randomDeck[nextCardFlag]);
+                    const div = document.getElementById(players[currentPlayer].id);
+                    const _hand = document.createElement('div');
+                    _hand.setAttribute("class", `carta ${randomDeck[nextCardFlag]}`)
+                    div?.appendChild(_hand);
+                    nextPlayer();
+                    nextCardFlag++;
+                  }
+                );
+
+function nextPlayer() {
+  currentPlayer++;
+  if(currentPlayer === players.length) {
+    currentPlayer = 0;
+  }
+}
