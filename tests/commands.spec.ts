f2ad277b629c362a265964eca6f8d5d0cd116117
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-04-16 14:19:18 -0300

    refactor: agrega tests al momento de armar el desk

--- tests/commands.spec.ts
+++ tests/commands.spec.ts
@@ -69,6 +69,41 @@ describe("BuildDeckCommand", () => {
     expect(spy).toBeCalled();
     expect(state.deck.cards.length).toBeGreaterThan(0);
   });
+
+  it("should generate a deck with a specific amount of cards", () => {
+    const command = new BuildDeckCommand();
+    const state = new GameState();
+
+    command.execute(state);
+
+    const greenCards = state.deck.cards.filter(
+      (card) => card.color === Color.GREEN
+    );
+    const blueCards = state.deck.cards.filter(
+      (card) => card.color === Color.BLUE
+    );
+    const redCards = state.deck.cards.filter(
+      (card) => card.color === Color.RED
+    );
+    const yellowCards = state.deck.cards.filter(
+      (card) => card.color === Color.YELLOW
+    );
+
+    const wildcards = state.deck.cards.filter(
+      (card) => card.value === Value.WILDCARD
+    );
+
+    const plusFour = state.deck.cards.filter(
+      (card) => card.value === Value.PLUS_FOUR
+    );
+
+    expect(greenCards.length).toBe(26);
+    expect(blueCards.length).toBe(26);
+    expect(redCards.length).toBe(26);
+    expect(yellowCards.length).toBe(26);
+    expect(wildcards.length).toBe(2);
+    expect(plusFour.length).toBe(2);
+  });
 });
 
 describe("FinalizeTurnCommand", () => {
