4ae726c75d307b1ea8b53b9bf6765780329b3a94
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-04-17 17:48:06 -0300

    refactor: separa los files de tests

---
+++ tests/commands/add-players.command.spec.ts
@@ -0,0 +1,27 @@
+import { Player } from "../../src/models/player.model";
+import { AddPlayersCommand } from "../../src/commands/add-players.command";
+import { GameState } from "../../src/models/game-state.model";
+
+describe("AddPlayersCommand", () => {
+  it("should set players attribute when we create the command", () => {
+    const player1 = new Player("p1", "player 1", "avatar");
+    const player2 = new Player("p2", "player 2", "avatar");
+    const command = new AddPlayersCommand([player1, player2]);
+
+    expect(command.players).toBeDefined();
+    expect(command.players[0].id).toEqual("p1");
+  });
+
+  it("should add players to state when we execute the command", () => {
+    const player1 = new Player("p1", "player 1", "avatar");
+    const player2 = new Player("p2", "player 2", "avatar");
+    const command = new AddPlayersCommand([player1, player2]);
+    const state = new GameState();
+    const spy = spyOn(state.playersGroup, "addPlayers").and.callThrough();
+
+    command.execute(state);
+
+    expect(spy).toBeCalled();
+    expect(state.playersGroup.players.length).toBe(2);
+  });
+});
---
+++ tests/models/card.model.spec.ts
@@ -0,0 +1,69 @@
+import { Card } from "../../src/models/card.model";
+import { Value } from "../../src/models/values.model";
+import { Color } from "../../src/models/color.model";
+
+describe("Card", () => {
+  it("should set sprite attribute when we create a card", () => {
+    const card = new Card(Value.ONE, Color.BLUE);
+
+    expect(card).toBeDefined();
+    expect(card.sprite).toEqual(`${Value.ONE}--${Color.BLUE}`);
+  });
+
+  it("should throw error if we try to create a wildcard with a color", () => {
+    expect(() => new Card(Value.WILDCARD, Color.BLUE)).toThrow(Error);
+  });
+
+  it("should return true when we invoke isSpecialCard and the card is special", () => {
+    const card = new Card(Value.WILDCARD);
+
+    expect(card.isSpecialCard()).toBeTruthy();
+  });
+
+  it("should return true when we invoke hasEffects and the card is has an effect", () => {
+    const card = new Card(Value.WILDCARD);
+
+    expect(card.hasEffects()).toBeTruthy();
+  });
+
+  it("should return true when we check if a wildcard is playable", () => {
+    const card = new Card(Value.THREE, Color.GREEN);
+    const wildcard = new Card(Value.WILDCARD);
+
+    expect(wildcard.isPlayable(card)).toBeTruthy();
+  });
+
+  it("should return specific score when we check the score of a card", () => {
+    const zero = new Card(Value.ZERO, Color.GREEN);
+    const one = new Card(Value.ONE, Color.GREEN);
+    const two = new Card(Value.TWO, Color.GREEN);
+    const three = new Card(Value.THREE, Color.GREEN);
+    const four = new Card(Value.FOUR, Color.GREEN);
+    const five = new Card(Value.FIVE, Color.GREEN);
+    const six = new Card(Value.SIX, Color.GREEN);
+    const seven = new Card(Value.SEVEN, Color.GREEN);
+    const eight = new Card(Value.EIGHT, Color.GREEN);
+    const nine = new Card(Value.NINE, Color.GREEN);
+    const plusTwo = new Card(Value.PLUS_TWO, Color.GREEN);
+    const reverse = new Card(Value.REVERSE, Color.GREEN);
+    const skip = new Card(Value.SKIP, Color.GREEN);
+    const wildcard = new Card(Value.WILDCARD);
+    const plusFour = new Card(Value.PLUS_FOUR);
+
+    expect(zero.score).toBe(0);
+    expect(one.score).toBe(1);
+    expect(two.score).toBe(2);
+    expect(three.score).toBe(3);
+    expect(four.score).toBe(4);
+    expect(five.score).toBe(5);
+    expect(six.score).toBe(6);
+    expect(seven.score).toBe(7);
+    expect(eight.score).toBe(8);
+    expect(nine.score).toBe(9);
+    expect(plusTwo.score).toBe(20);
+    expect(reverse.score).toBe(20);
+    expect(skip.score).toBe(20);
+    expect(wildcard.score).toBe(50);
+    expect(plusFour.score).toBe(50);
+  });
+});
