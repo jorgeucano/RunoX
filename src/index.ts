9c63ead5018e5f0da10ce0f432c6c6ad74022fc4
jorgeucano <jorgeucano@gmail.com>
2020-04-24 17:58:28 -0300

    add firebase

--- src/index.ts
+++ src/index.ts
@@ -188,3 +188,34 @@ function drawTurn(player: Player) {
 
   _turn?.appendChild(turnDiv);
 }
+
+const firebase = require('firebase');
+const firebaseui = require('firebaseui');
+const initializeFirebase = () => {
+   // TODO: Replace the following with your app's Firebase project configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyAZX1DilyM9IY01_xFa2pE4ull7FYOsQ00",
+      authDomain: "runox-card.firebaseapp.com",
+      databaseURL: "https://runox-card.firebaseio.com",
+      projectId: "runox-card",
+      storageBucket: "runox-card.appspot.com",
+      messagingSenderId: "608707088831",
+      appId: "1:608707088831:web:f204f1e44046d59d23d10a",
+      measurementId: "G-RVDLEJNBM9"
+    }
+    /*
+     * export const firebaseConfig = { ...firebase config... }
+     */
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); 
+  console.log('firebase');
+  firebaseLogin();
+}
+
+const firebaseLogin = () => {
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+}
+
+
+// inicializamos firebase 
+initializeFirebase();
