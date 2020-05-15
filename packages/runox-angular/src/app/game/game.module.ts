2bacff5ad8e961ea23449a7c4fc2098d24707ebf
Marcos Juan Juan <webbegg@gmail.com>
2020-05-15 20:46:20 +0200

    GameBoard WIP

--- packages/runox-angular/src/app/game/game.module.ts
+++ packages/runox-angular/src/app/game/game.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GameRoutingModule } from './game-routing.module';
+import {SharedModule} from '../shared/shared.module';
 import { GameComponent } from './game.component';
+import { GameBoardComponent } from './components/game-board/game-board.component';
 
 
 @NgModule({
-  declarations: [GameComponent],
-  imports: [
-    CommonModule,
-    GameRoutingModule
+  declarations: [GameComponent, GameBoardComponent], imports: [
+    CommonModule, GameRoutingModule, SharedModule
   ]
 })
 export class GameModule { }
