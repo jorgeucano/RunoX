0614b84ba527b65aec9619dda7671347f3a7b9ae
AlexScigalszky <alexpablo.90@gmail.com>
2020-08-24 21:37:29 -0300

    implement hasToSelectColor

--- packages/runox-angular/src/app/game/components/game-board/game-board.component.ts
+++ packages/runox-angular/src/app/game/components/game-board/game-board.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Room } from 'src/app/models/room';
 import { IPlayer } from '@runox-game/game-engine/lib/models/player.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Value } from '@runox-game/game-engine/lib/models/values.model';
 
 @Component({
   selector: 'rnx-game-board',
@@ -13,4 +16,12 @@ export class GameBoardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  get hasToSelectColor(): Observable<boolean> {
+    return this.room
+      .onCardPlayed()
+      .pipe(
+        map((x) => x?.value === Value.WILDCARD || x?.value === Value.PLUS_FOUR)
+      );
+  }
 }
