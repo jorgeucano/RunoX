5bf8b82e6a9e6a37f97b8f2b6b77575e90826e9b
AlexScigalszky <alexpablo.90@gmail.com>
2020-09-10 18:10:53 -0300

    various

--- packages/runox-angular/src/app/game/components/deck/deck.component.ts
+++ packages/runox-angular/src/app/game/components/deck/deck.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'rnx-deck',
   templateUrl: './deck.component.html',
   styleUrls: ['./deck.component.css'],
 })
-export class DeckComponent implements OnInit {
-  constructor() {}
+export class DeckComponent {
 
-  ngOnInit(): void {}
+  takeCard(): void {}
 }
